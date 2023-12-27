gregdaynes/hyde-page-css

commit fa997756cfb0acdab8bc1759960d1a5a1e46485d

refactor!: complete rework
The initial implementation hacked up core Jekyll classes which would
cause rendering conflicts and unstable conditions.

A new approach fitting entirely within the Page Pre-Render hook, we can
achieve the same result in a simpler and more stable way.

example/_layouts/default.html
@@ -6,7 +6,7 @@ css:
 <!doctype html>
 <html>
 <head>
-	{% for file in page.css_files -%}
+	{% for file in site.data.css_files -%}
 	<link rel="stylesheet" href="{{ file | prepend: '/' | prepend: site.baseurl }}">
 	{% endfor %}
 </head>

lib/hyde-page-css.rb
@@ -1,11 +1,212 @@
+require 'jekyll'
+require 'digest'
+
+Jekyll::Hooks.register :pages, :pre_render do |page, payload|
+  Hyde::Page::Css.new(page).run
+end
+
 module Hyde
   module Page
     class Css
-      VERSION = "0.3.0"
+      VERSION = "0.4.0"
+    end
+
+    class GeneratedCssFile < Jekyll::StaticFile
+      attr_accessor :file_contents
+      attr_reader :generator
+
+      def initialize(site, dir, name)
+        @site = site
+        @dir = dir
+        @name = name
+        @relative_path = File.join(*[@dir, @name].compact)
+        @extname = File.extname(@name)
+        @type = @collection&.label&.to_sym
+        @generator = "hyde-page-css"
+      end
+
+      def write(dest)
+        dest_path = destination(dest)
+        return false if File.exist?(dest_path)
+
+        FileUtils.mkdir_p(File.dirname(dest_path))
+        FileUtils.rm(dest_path) if File.exist?(dest_path)
+
+        File.open(dest_path, "w") do |output_file|
+          output_file << file_contents
+        end
+
+        true
+      end
+    end
+
+    class Css
+      @@config = {
+        "source" => "assets/css",
+        "destination" => "assets/css",
+        "minify" => true,
+        "enable" => true,
+        "keep_files" => true,
+        "dev_mode" => false
+      }
+
+      def initialize(page)
+        @page = page
+        @site = page.site
+        @config = fetch_config
+        @site.data['_hyde_pages_cache'] ||= {}
+
+        if keep_files? && !dev_mode?
+          @site.config.fetch("keep_files").push(destination)
+        end
+      end
+
+      def run
+        css = fetch_css(@page)
+        layout = fetch_layout(fetch_layout_name(@page))
+        results = parent_layout_css(layout, css).reverse
+        return if results.empty?
+
+        lookup_name = names_to_key(results)
+        return unless @site.data['_hyde_pages_cache'].fetch(lookup_name, nil).nil?
+
+        data = concatenate_files(results)
+        return if data == ""
+
+        data = minify(data)
+        return if data == ""
+
+        generated_file = generate_file(results, data)
+
+        # file already exists, so skip writing out the data to disk
+        return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
+
+        # place file data into the new file
+        generated_file.file_contents = data
+
+        # assign static file to list for jekyll to render
+        @site.static_files << generated_file
+
+        # assign to site.data.css_files for liquid output
+        add_to_urls(generated_file.url)
+
+        # add to cache
+        @site.data['_hyde_pages_cache'][lookup_name] = data
+      end
+
+      private
+
+      def names_to_key(names)
+        names.join('-')
+      end
+
+      def add_to_urls(url)
+        @site.data['css_files'] ||= []
+        @site.data['css_files'].push(url)
+      end
+
+      def fetch_config
+        @@config.merge(@site.config.fetch("hyde_page_css", {}))
+      end
+
+      def keep_files?
+        @config.fetch("keep_files") == true
+      end
+
+      def dev_mode?
+        @config.fetch("dev_mode") == true
+      end
+
+      def minify?
+        @config.fetch("minify") == true
+      end
+
+      def destination
+        @config.fetch("destination")
+      end
+
+      def source
+        @config.fetch("source")
+      end
+
+      def qualified_source
+        File.join(*[@site.source, source].compact)
+      end
+
+      def fetch_layout_name(obj_with_data, default = nil)
+        obj_with_data.data.fetch('layout', default)
+      end
+
+      def fetch_css(obj_with_data, default = [])
+        obj_with_data.data.fetch('css', []).reverse
+      end
+
+      def fetch_layout(layout_name, default = nil)
+        @site.layouts.fetch(layout_name, default)
+      end
+
+      def parent_layout_css(layout, css)
+        if layout.nil?
+          return css.uniq.compact
+        end
+
+        layout_name = fetch_layout_name(layout)
+        parent_layout = fetch_layout(layout_name)
+        css = css.concat(fetch_css(layout))
+
+        parent_layout_css(parent_layout, css)
+      end
+
+      def concatenate_files(files, data = [])
+        files.each do |file_name|
+          # tmp page required to handle anything with frontmatter/yaml header
+          tmp_page = Jekyll::PageWithoutAFile.new(@site, nil, source, file_name)
+          path = File.join([qualified_source, file_name])
+
+          begin
+            tmp_page.content = File.read(path)
+            data.push(Jekyll::Renderer.new(@site, tmp_page).run)
+          rescue
+            Jekyll.logger.warn("Page CSS Warning:", "Unable to find #{path}")
+          end
+        end
+
+        data.join("\n")
+      end
+
+      def style
+        if dev_mode?
+          "expanded"
+        elsif !minify?
+          "expanded"
+        else
+          "compressed"
+        end
+      end
+
+      def minify(data)
+        return data unless minify?
+
+        converter_config = {"sass" => {"style" => style}}
+        Jekyll::Converters::Scss.new(converter_config).convert(data)
+      end
+
+      def generate_file(files, data)
+        file_name = generate_file_name(files, data)
+        Hyde::Page::GeneratedCssFile.new(@site, source, file_name)
+      end
+
+      def generate_file_name(files, data, prefix: nil)
+        file_names = [prefix]
+
+        if dev_mode?
+          files.each { |file| file_names.push(file.gsub(".css", "")) }
+        end
+
+        file_names.push(Digest::MD5.hexdigest(data)[0, 6])
+
+        file_names.compact.join("-") + ".css"
+      end
     end
   end
 end
-
-require_relative "hyde/renderer.rb"
-require_relative "hyde/page/css.rb"
-require_relative "hyde/page/generated_page_css_file.rb"

example/Gemfile
@@ -16,7 +16,7 @@ gem "jekyll", "~> 4.3.2"
 # If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
-  gem "hyde-page-css", "~> 0.3"
+  gem "hyde-page-css", :path => "../"
 end
 
 # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem

hyde-page-css.gemspec
@@ -1,4 +1,4 @@
-require File.expand_path("../lib/hyde-page-css.rb", __FILE__)
+require File.expand_path("./lib/hyde-page-css.rb")
 
 Gem::Specification.new do |s|
   s.name = "hyde-page-css"

lib/hyde/page/css.rb
@@ -1,131 +1,0 @@
-require 'jekyll'
-require_relative "generated_page_css_file"
-
-module Hyde
-  module Page
-    class Css
-      @@config = {
-        "asset_path" => "assets/css",
-        "file_output_path" => "assets/css",
-        "css_minify" => true,
-        "enable" => true,
-        "keep_files" => true,
-        "dev_mode" => false
-      }
-
-      def initialize(layout, info)
-        @site = info[:registers][:site]
-        @page = info[:registers][:page]
-        @data = layout.data
-        @config = @@config.merge(@site.config.dig("hyde_page_css") || {})
-
-        @qualified_asset_path = File.join(*[@site.source, @config["asset_path"]].compact)
-
-        if config("keep_files") == true && config("dev_mode") == false
-          @site.config["keep_files"].push(config("file_output_path"))
-        end
-
-        @page["css"] = if @page["css"].nil?
-          []
-        else
-          [@page["css"]]
-        end
-      end
-
-      def generate
-        return unless config("enable") == true
-        @page["css"].unshift(@data["css"])
-
-        file_groups = flatten_group(@page["css"])
-
-        css_files = []
-
-        for files in file_groups do
-          break if !files&.length
-
-          data = concatenate_files(files.compact)
-          data = minify(data)
-          next if data == ""
-
-          file = generate_file(files, data)
-          css_files << file.url
-
-          # file already exists, so skip writing out the data to disk
-          next if @site.static_files.find { |x| x.name == file.name }
-
-          # place file data into the new file
-          file.file_contents = data
-
-          # assign static file to list for jekyll to render
-          @site.static_files << file
-        end
-
-        # the recursive nature of this will sometimes have duplicate css files
-        @page["css_files"] = css_files.uniq
-      end
-
-      private
-
-      def config(*)
-        @config.dig(*)
-      end
-
-      def flatten_group(arr, acc = [])
-        return [arr] if !arr.last.is_a?(Array)
-
-        acc += [arr.first]
-        acc += flatten_group(arr.last)
-      end
-
-      def concatenate_files(files, data = "")
-        for file in files do
-          # tmp page required to handle anything with frontmatter/yaml header
-          tmp_page = Jekyll::PageWithoutAFile.new(@site, nil, config("asset_path"), file)
-          path = File.join([@qualified_asset_path, file])
-
-          begin
-            file_contents = File.read(path)
-            tmp_page.content = file_contents
-            # original jekyll renderer, not our modified version
-            data << Jekyll::Renderer.new(@site, tmp_page).run
-          rescue
-            Jekyll.logger.warn("Page CSS Warning:", "Unable to find #{path}")
-          end
-        end
-
-        data
-      end
-
-      def minify(data)
-        style = if config("dev_mode") == true
-          "expanded"
-        elsif config("css_minify") == false
-          "expanded"
-        else
-          "compressed"
-        end
-
-        converter_config = {"sass" => {"style" => style}}
-        Jekyll::Converters::Scss.new(converter_config).convert(data)
-      end
-
-      def generate_file(files, data)
-        file_name = generate_file_name(files, data)
-
-        Hyde::Page::GeneratedPageCssFile.new(@site, config("asset_path"), file_name)
-      end
-
-      def generate_file_name(files, data, prefix: nil)
-        file_names = [prefix]
-
-        if config("dev_mode")
-          files.each { |file| file_names.push(file.gsub(".css", "")) }
-        end
-
-        file_names.push(Digest::MD5.hexdigest(data)[0, 6])
-
-        file_names.compact.join("-") + ".css"
-      end
-    end
-  end
-end

lib/hyde/page/generated_page_css_file.rb
@@ -1,35 +1,0 @@
-module Hyde
-  module Page
-    # Alternative class for jekyll's static files
-    # this allows the creation of files without a source file
-
-    class GeneratedPageCssFile < Jekyll::StaticFile
-      attr_accessor :file_contents
-      attr_reader :generator
-
-      def initialize(site, dir, name)
-        @site = site
-        @dir = dir
-        @name = name
-        @relative_path = File.join(*[@dir, @name].compact)
-        @extname = File.extname(@name)
-        @type = @collection&.label&.to_sym
-        @generator = "hyde-page-css"
-      end
-
-      def write(dest)
-        dest_path = destination(dest)
-        return false if File.exist?(dest_path)
-
-        FileUtils.mkdir_p(File.dirname(dest_path))
-        FileUtils.rm(dest_path) if File.exist?(dest_path)
-
-        File.open(dest_path, "w") do |output_file|
-          output_file << file_contents
-        end
-
-        true
-      end
-    end
-  end
-end

lib/hyde/renderer.rb
@@ -1,18 +1,0 @@
-module Jekyll
-  class Renderer
-    def render_layout(output, layout, info)
-      Hyde::Page::Css.new(layout, info).generate
-
-      # TODO Would be nice to call super here instead of cloned logic from Jekyll internals
-      payload["content"] = output
-      payload["layout"] = Utils.deep_merge_hashes(layout.data, payload["layout"] || {})
-
-      render_liquid(
-        layout.content,
-        payload,
-        info,
-        layout.path
-      )
-    end
-  end
-end

commit baabf72c8c21cb8c61443d552593c909fa40b310

fix!: regression that combined all css files
The refactoring in fa997756 introduced a regression where instead of
combining css on a per-layout bases and exposing those to the page, the
change combined all the css per page and put it on every page.

This change should restore original functionality where a nested layout
page eg:
- base.html: css-a, css-b
  - home: css-c

Would add an array to the `page` data containing 2 entries
1. `assets/css/css-a-css-b.css`
2. `assets/css/css-c`

example/_layouts/default.html
@@ -6,7 +6,7 @@ css:
 <!doctype html>
 <html>
 <head>
-	{% for file in site.data.css_files -%}
+	{% for file in page.css_files -%}
 	<link rel="stylesheet" href="{{ file | prepend: '/' | prepend: site.baseurl }}">
 	{% endfor %}
 </head>

lib/hyde-page-css.rb
@@ -1,8 +1,8 @@
 require 'jekyll'
 require 'digest'
 
-Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  Hyde::Page::Css.new(page).run
+Jekyll::Hooks.register :pages, :post_init do |page|
+  Hyde::Page::Css.new(page).run if page.instance_of? Jekyll::Page
 end
 
 module Hyde
@@ -64,34 +64,42 @@ module Hyde
       def run
         css = fetch_css(@page)
         layout = fetch_layout(fetch_layout_name(@page))
-        results = parent_layout_css(layout, css).reverse
-        return if results.empty?
+        css_groups = parent_layout_css(layout, css).reverse
+        return if css_groups.empty?
 
-        lookup_name = names_to_key(results)
-        return unless @site.data['_hyde_pages_cache'].fetch(lookup_name, nil).nil?
+        for group in css_groups
+          lookup_name = names_to_key(group)
+          cache_entry = @site.data['_hyde_pages_cache'].fetch(lookup_name, nil)
 
-        data = concatenate_files(results)
-        return if data == ""
+          if cache_entry.nil?
+            data = concatenate_files(group)
+            return if data == ""
 
-        data = minify(data)
-        return if data == ""
+            data = minify(data)
+            return if data == ""
 
-        generated_file = generate_file(results, data)
+            generated_file = generate_file(group, data)
 
-        # file already exists, so skip writing out the data to disk
-        return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
+            # file already exists, so skip writing out the data to disk
+            return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
 
-        # place file data into the new file
-        generated_file.file_contents = data
+            # place file data into the new file
+            generated_file.file_contents = data
 
-        # assign static file to list for jekyll to render
-        @site.static_files << generated_file
+            # assign static file to list for jekyll to render
+            @site.static_files << generated_file
 
-        # assign to site.data.css_files for liquid output
-        add_to_urls(generated_file.url)
+            # add to cache
+            cache_entry = {
+              "url": generated_file.url,
+              "data": data
+            }
+            @site.data['_hyde_pages_cache'][lookup_name] = cache_entry
+          end
 
-        # add to cache
-        @site.data['_hyde_pages_cache'][lookup_name] = data
+          # assign to page.data.css_files for liquid output
+          add_to_urls(cache_entry.fetch(:url, nil)).compact
+        end
       end
 
       private
@@ -101,8 +109,8 @@ module Hyde
       end
 
       def add_to_urls(url)
-        @site.data['css_files'] ||= []
-        @site.data['css_files'].push(url)
+        @page.data['css_files'] ||= []
+        @page.data['css_files'].push(url)
       end
 
       def fetch_config
@@ -138,7 +146,7 @@ module Hyde
       end
 
       def fetch_css(obj_with_data, default = [])
-        obj_with_data.data.fetch('css', []).reverse
+        [obj_with_data.data.fetch('css', []).reverse]
       end
 
       def fetch_layout(layout_name, default = nil)
@@ -147,7 +155,7 @@ module Hyde
 
       def parent_layout_css(layout, css)
         if layout.nil?
-          return css.uniq.compact
+          return css
         end
 
         layout_name = fetch_layout_name(layout)