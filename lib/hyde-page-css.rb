664b2b5b0ade1964bfdc73ec90eb8e5f35951f67
Gregory Daynes <email@gregdaynes.com>
2024-10-25 11:13:23 -0700

    fix: skip empty css groups

--- lib/hyde-page-css.rb
+++ lib/hyde-page-css.rb
@@ -72,7 +72,9 @@ module Hyde
         return if css_groups.flatten.empty?
 
         for group in css_groups
+          next if group.nil?
           next if group.empty?
+
           lookup_name = names_to_key(group)
           cache_entry = @site.data["_hyde_pages_cache"].fetch(lookup_name, nil)
 
